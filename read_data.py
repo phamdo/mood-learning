6f4d23a3aee74faeb8d76029978d5fc1e0bd90c3
phamdo <kaman@phamdo.com>
2016-07-19 22:54:34 -0400

    Added to read_data

--- read_data.py
+++ read_data.py
@@ -1,11 +1,13 @@
+# use 'python read_data format raw_data.csv mood_data.csv' to format data
+
 import csv
 import sys
 
-# read data
-def read_data():
-    with open(sys.argv[1], 'rb') as csvfile:
+# processes raw data and strips unnecessary information (notes)
+def format_data():
+    with open(sys.argv[2], 'rb') as csvfile:
         reader = csv.reader(csvfile, delimiter = ',', quotechar = '|')
-        output = open(sys.argv[2], 'wt')
+        output = open(sys.argv[3], 'wt')
         writer = csv.writer(output)
         for row in reader:
             new_row = []
@@ -14,5 +16,16 @@ def read_data():
             print new_row
             writer.writerow(new_row)
 
+def read_data():
+    data = {}
+    with open(sys.argv[2], 'rb') as csvfile:
+        reader = csv.DictReader(csvfile, delimiter = ',', quotechar = '|')
+        for row in reader:
+            data[row['activities']] = data[row['mood']]
+    return data
 
-read_data()
+if (sys.argv[1] == "format"):
+    format_data()
+elif (sys.argv[1] == "read"):
+    print read_data()
+    
