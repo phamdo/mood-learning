1836aa89f7add93e7a841e3f3c84139228abe340
phamdo <kaman@phamdo.com>
2016-07-26 22:52:07 -0400

    Implemented nearest neighbor classifier algorithm

--- nearest_neighbor.py
+++ nearest_neighbor.py
@@ -1,36 +1,40 @@
 from read_data import *
+from math import ceil
+def nearest_neighbor(entries, item):   
+    minDist = 1
+    minEntries = []
+    for entry in entries:
+        d = dist(item.get_activities(), entry.get_activities())
+        if d < minDist:
+            minDist = d
+            minEntries = []
+            minEntries.append(entry)
+        elif d == minDist:
+            minEntries.append(entry)
+    total = 0
+    print "nearest neighbors : ", minEntries
+    for entry in minEntries:
+        total += mood_rating(entry.get_mood())
+    return mood(ceil(total / len(minEntries)))
+    
 
-def nearest_neighbor(datafile, inputfile):
-    entries = read_data(datafile)
-    inputs = read_data(inputfile)
-    for item in inputs:
-        minDist = 1
-        minEntries = []
-        for entry in entries:
-            d = dist(item.get_activities(), entry.get_activities())
-            if d < minDist:
-                minDist = d
-                minEntries = []
-                minEntries.append(entry)
-            elif d == minDist:
-                minEntries.append(entry)
-        print minEntries
 
-
-
-def dist(input1, input2):
+def dist(list1, list2):
     dist = 0
     total = 0
-    list2 = lis2
+    same = 0
     for item in list1:
         if item not in list2:
             dist += 1
         else:
-            list2.remove(item)
+            same += 1
         total += 1
-    dist += len(list2)
+    dist += (len(list2) - same)
     total += len(list2)
-    print dist, total
     return dist/float(total)
 
-nearest_neighbor(sys.argv[1], sys.argv[2])
+entries = read_data(sys.argv[1])
+inputs = read_data(sys.argv[2])
+for item in inputs:
+    print item
+    print nearest_neighbor(entries, item)
--- read_data.py
+++ read_data.py
@@ -1,6 +1,5 @@
 import csv
 import sys
-#from enum import Enum
 
 class Date:
     def __init__(self, year, day):
@@ -9,13 +8,6 @@ class Date:
     def __str__(self):
         return str(self.year) + "," + self.day
 
-# an ordinal measure of mood
-#class Mood(Enum):
-#    "awful" = 1
-#    "not great" = 2
-#    "meh" = 3
-#    "good" = 4
-#    "rad" = 5
 
 class Entry:
     def __init__(self, year, day, time, mood, activities):
@@ -34,8 +26,20 @@ class Entry:
     def get_activities(self):
         return self.activities
 
+    def get_mood(self):
+        return self.mood
+
 # processes raw data from 'rawfile' and strips unnecessary information 
-# (notes) and writes the formatted data to 'datafile'
+# an ordinal measure of mood
+def mood_rating(mood):
+    mood_dict = {"awful" : 1, "not great" : 2, "meh" : 3, "good" : 4, "rad" : 5};
+    return mood_dict[mood]
+
+def mood(rating):
+    mood_dict = {1 : "awful", 2 : "not great", 3 : "meh", 4 : "good", 5 : "rad"}
+    return mood_dict[rating]
+
+# takes in raw data, removes unwanted information (notes) and writes the formatted data to 'datafile'
 def format_data(rawfile, datafile):
     with open(rawfile, 'rb') as csvfile:
         reader = csv.reader(csvfile, delimiter = ',', quotechar = '|')
@@ -57,29 +61,7 @@ def read_data(filename):
             entry = Entry(row[0], row[1], row[2], row[3], row[4])
             data.append(entry)
     return data
-    #data = {}
-    #with open(sys.argv[2], 'rb') as csvfile:
-    #    reader = csv.reader(csvfile, delimiter = ',', quotechar = '|')
-    #    for row in reader:
-    #        if (data.get(row[3]) != None):
-    #            data[row[3]].append(row[4].split(" | "))
-    #        else:
-    #            data[row[3]] = [row[4]]
-    #return data
-
-
-# distance between any two entries is the number of activities that differ
-def distance(list1, list2):
-    dist = 0
-    for item in list1:
-        if item not in list2:
-            dist += 1
-        else:
-            list2.remove(item)
-    dist += len(list2)
-    return dist
-
-
+    
 # use 'python read_data.py format raw_data.csv mood_data.csv' to format data
 if (sys.argv[1] == "format"):
     format_data(sys.argv[2], sys.argv[3])
